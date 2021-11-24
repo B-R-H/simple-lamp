9faa3f30a94f92dc3a067af01c01151267f705a2
Barnaby_R_H <barnaby.robertson-hurst@associates.ecs.co.uk>
2021-11-24 11:22:52 +0000

    base tf code

---
+++ Infra/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+  backend "s3" {
+    bucket = "my-tf-test-bucket-testing"
+    key    = "global/s3/container-project/terraform.tfstate"
+    region = "eu-west-2"
+
+    dynamodb_table = "terraform-up-and-runing-state-locks"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
